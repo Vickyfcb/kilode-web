6ddaf17d7f0a7314d30f30656feb730dfcbd2892
Victor code <okokonvictor67@gmail.com>
2022-10-12 14:13:10 +0100

    Add files via upload

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,11 +1,27 @@
-// ACTIVE LINK
-var header = document.getElementById("nav-list");
-var activeLink = header.getElementsByClassName("active-link");
-for (var i = 0; i < activeLink.length; i++) {
-  activeLink[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active-link");
-  current[0].className = current[0].className.replace(" active-link", "");
-  this.className += " active-link";
-  });
+// SHOW MENU
+const showMenu = (toggleId, navbarId,bodyId) =>{
+  const toggle = document.getElementById(toggleId),
+  navbar = document.getElementById(navbarId),
+  bodypadding = document.getElementById(bodyId)
+
+  if(toggle && navbar){
+      toggle.addEventListener('click', ()=>{
+          // APARECER MENU
+          navbar.classList.toggle('show')
+          // ROTATE TOGGLE
+          toggle.classList.toggle('rotate')
+          // PADDING BODY
+          bodypadding.classList.toggle('expander')
+      })
+  }
+}
+showMenu('nav-toggle','navbar','body')
+
+// LINK ACTIVE COLOR
+const linkColor = document.querySelectorAll('.nav__link');   
+function colorLink(){
+  linkColor.forEach(l => l.classList.remove('active'));
+  this.classList.add('active');
 }
 
+linkColor.forEach(l => l.addEventListener('click', colorLink));
