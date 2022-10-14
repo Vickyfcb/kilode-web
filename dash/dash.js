faa1a1164a5e8cb875c19131443bc031d5725494
Victor code <okokonvictor67@gmail.com>
2022-10-14 09:39:55 +0100

    Add files via upload

---
+++ dash/dash.js
@@ -0,0 +1,35 @@
+const navMenu = document.getElementById('chat-box'),
+      navToggle = document.getElementById('chat-open'),
+      navClose = document.getElementById('chat-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+const optionMenu = document.querySelector(".select-menu"),
+        selectBtn = optionMenu.querySelector(".select-btn"),
+        options = optionMenu.querySelectorAll(".option"),
+        sBtn_text = optionMenu.querySelector(".sBtn-text");
+
+        selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));       
+
+        options.forEach(option =>{
+        option.addEventListener("click", ()=>{
+        let selectedOption = option.querySelector(".option-text").innerText;
+        sBtn_text.innerText = selectedOption;
+
+        optionMenu.classList.remove("active");
+          });
+        });
