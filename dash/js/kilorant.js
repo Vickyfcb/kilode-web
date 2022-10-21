aa6f510e6189937f9cefef9023d1a014305edcf6
Victor code <okokonvictor67@gmail.com>
2022-10-22 00:57:04 +0100

    Add files via upload

---
+++ dash/js/kilorant.js
@@ -0,0 +1,19 @@
+const chatBox = document.getElementById('notifications-menu'),
+      chatOpen = document.getElementById('notifications-open'),
+      chatClose = document.getElementById('notifications-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(chatOpen){
+    chatOpen.addEventListener('click', () =>{
+        chatBox.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(chatClose){
+    chatClose.addEventListener('click', () =>{
+        chatBox.classList.remove('show-menu')
+    })
+}
