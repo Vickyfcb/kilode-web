6b4bd74c26ab0650b8f13184f96fb43a5690ecab
Cyber VC <okokonvictor67@gmail.com>
2022-10-30 21:56:16 -0700

    Add files via upload

---
+++ js/user-dash.js
@@ -0,0 +1,99 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-second'),
+navToggle = document.getElementById('navsecond-toggle'),
+navClose = document.getElementById('navsecond-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+navToggle.addEventListener('click', () =>{
+  navMenu.classList.add('show-menu')
+})
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+navClose.addEventListener('click', () =>{
+  navMenu.classList.remove('show-menu')
+})
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link, .logout__link')
+
+function linkAction(){
+const navMenu = document.getElementById('nav-second')
+// When we click on each nav__link, we remove the show-menu class
+navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// SHOW MENU
+const showMenu = (toggleId, navbarId,bodyId) =>{
+  const toggle = document.getElementById(toggleId),
+  navbar = document.getElementById(navbarId),
+  bodypadding = document.getElementById(bodyId)
+
+  if(toggle && navbar){
+      toggle.addEventListener('click', ()=>{
+          // APARECER MENU
+          navbar.classList.toggle('show')
+          // ROTATE TOGGLE
+          toggle.classList.toggle('rotate')
+          // PADDING BODY
+          bodypadding.classList.toggle('expander')
+      })
+  }
+}
+showMenu('nav-toggle','navbar','body')
+
+// LINK ACTIVE COLOR
+const linkColor = document.querySelectorAll('.nav__link');   
+function colorLink(){
+  linkColor.forEach(l => l.classList.remove('active'));
+  this.classList.add('active');
+}
+
+linkColor.forEach(l => l.addEventListener('click', colorLink));
+
+// MESSAGES
+const chatBox = document.getElementById('chat-box'),
+      chatOpen = document.getElementById('chat-open'),
+      chatClose = document.getElementById('chat-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(chatOpen){
+    chatOpen.addEventListener('click', () =>{
+        chatBox.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(chatClose){
+    chatClose.addEventListener('click', () =>{
+        chatBox.classList.remove('show-menu')
+    })
+}
+//NOTIFICATIONS
+const notificationBox = document.getElementById('notifications-menu'),
+      notificationOpen = document.getElementById('notifications-open'),
+      notificationClose = document.getElementById('notifications-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(notificationOpen){
+    notificationOpen.addEventListener('click', () =>{
+        notificationBox.classList.add('show-notifications')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(notificationClose){
+    notificationClose.addEventListener('click', () =>{
+        notificationBox.classList.remove('show-notifications')
+    })
+}
