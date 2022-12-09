049d2955bcaf4356ce71ca69704061140b51c00c
Cyber VC <okokonvictor67@gmail.com>
2022-12-09 09:34:22 +0100

    Add files via upload

--- js/user-dash.js
+++ js/user-dash.js
@@ -198,33 +198,5 @@ skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
 })
 
-const selectAll = document.querySelector('.setup-group.select-all input');
-const allCheckbox = document.querySelectorAll('.setup-group:not(.select-all) input');
-let listBoolean = [];
-
-allCheckbox.forEach(item=> {
-	item.addEventListener('change', function () {
-		allCheckbox.forEach(i=> {
-			listBoolean.push(i.checked);
-		})
-		if(listBoolean.includes(false)) {
-			selectAll.checked = false;
-		} else {
-			selectAll.checked = true;
-		}
-		listBoolean = []
-	})
-})
-
-selectAll.addEventListener('change', function () {
-	if(this.checked) {
-		allCheckbox.forEach(i=> {
-			i.checked = true;
-		})
-	} else {
-		allCheckbox.forEach(i=> {
-			i.checked = false;
-		})
-	}
-})
+//select
 
